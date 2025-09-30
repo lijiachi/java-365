f81ccc909796b25fefc474c56b0a235113702e10
Chandler <18461031174@163.com>
2025-09-30 15:01:28 +0800

    day001: 添加JUnit5依赖 + 通过第1个单测

---
+++ src/main/java/com/java365/Day001.java
@@ -0,0 +1,11 @@
+package com.java365;
+
+public class Day001 {
+    public static String greeting() {
+        return "java-365 start";
+    }
+    public static void main(String[] args) {
+        System.out.println(greeting());
+    }
+}
+
---
+++ src/test/java/com/java365/Day001Test.java
@@ -0,0 +1,11 @@
+package com.java365;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class Day001Test {
+    @Test
+    public void greeting() {
+        assertEquals("java-365 start", Day001.greeting());
+    }
+}
+
