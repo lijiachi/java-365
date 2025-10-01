b1ec18b79f0adee0f12d3b993d8c8c4e58844146
Chandler <18461031174@163.com>
2025-10-01 08:12:05 +0800

    第二天的绿点完成，后续会增加逻辑

---
+++ src/main/java/com/java365/Day002.java
@@ -0,0 +1,10 @@
+package com.java365;
+
+public class Day002 {
+    public static String greeting() {
+        return "java365-第2天";
+    }
+    public static void main(String[] args) {
+        System.out.println(greeting());
+    }
+}
---
+++ src/test/java/com/java365/Day002Test.java
@@ -0,0 +1,12 @@
+package com.java365;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day002Test {
+    @Test
+    public void greeting() {
+        assertEquals("java365-第2天", Day002.greeting());
+    }
+}
