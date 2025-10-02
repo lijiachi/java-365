1148af4bc9aa143d0822f850c38ae9f095d34def
Chandler <18461031174@163.com>
2025-10-02 12:29:03 +0800

    第三天的绿点承包商

---
+++ src/main/java/com/java365/Day003.java
@@ -0,0 +1,8 @@
+package com.java365;
+
+public class Day003 {
+    public static String greeting() {
+        return "java365-第3天";
+    }
+
+}
---
+++ src/test/java/com/java365/Day003Test.java
@@ -0,0 +1,10 @@
+package com.java365;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Day003Test {
+    @Test
+    public void testGreeting() {
+        assertEquals("java365-第3天", Day003.greeting());
+    }
+}
