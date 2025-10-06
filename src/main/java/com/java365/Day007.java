dbcc4fea3eedddd89d043b887ef431dc7ad418b0
Chandler <18461031174@163.com>
2025-10-06 19:23:04 +0800

    点亮绿点

---
+++ src/main/java/com/java365/Day007.java
@@ -0,0 +1,7 @@
+package com.java365;
+
+public class Day007 {
+    public static String greeting(){
+        return "java-365第7天";
+    }
+}
---
+++ src/test/java/com/java365/Day007Test.java
@@ -0,0 +1,11 @@
+package com.java365;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day007Test {
+    @Test
+    public void testGreeting(){
+        assertEquals("java-365第7天",Day007.greeting());}
+}
