1daa7717d551a8d9df0eee997c8d3bfd1f293f71
Chandler <18461031174@163.com>
2025-10-09 07:50:50 +0800

    点亮绿点,仅写一个简单的测试,具体学习内容留白

---
+++ src/main/java/com/java365/Day010.java
@@ -0,0 +1,7 @@
+package com.java365;
+
+public class Day010 {
+    public static String greeting() {
+        return"java-365第10天";
+    }
+}
---
+++ src/test/java/com/java365/Day010Test.java
@@ -0,0 +1,12 @@
+package com.java365;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day010Test {
+    @Test
+    public void testGreeting(){
+        assertEquals("java-365第10天",Day010.greeting());
+    }
+}
