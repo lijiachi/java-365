fef14e13b8c19483be15e692af187de3483b9ec4
Chandler <18461031174@163.com>
2025-10-08 17:55:50 +0800

    点亮绿点

---
+++ src/main/java/com/java365/Day009.java
@@ -0,0 +1,7 @@
+package com.java365;
+
+public class Day009 {
+    public static String greeting(){
+        return "java-365第9天";
+    }
+}
---
+++ src/test/java/com/java365/Day009Test.java
@@ -0,0 +1,12 @@
+package com.java365;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day009Test {
+    @Test
+    public void testGreeting(){
+        assertEquals("java-365第9天", Day009.greeting());
+    }
+}
