ef9111ae125924236255b6d6d0e2ea8754ebc094
lijiachi <18461031174@163.com>
2025-10-10 07:55:23 +0800

    点亮绿点，主内容和JVM均待补充内容

---
+++ src/main/java/com/java365/Day011.java
@@ -0,0 +1,7 @@
+package com.java365;
+
+public class Day011 {
+    public static String greeting(){
+        return"java-365第11天";
+    }
+}
---
+++ src/test/java/com/java365/Day011Test.java
@@ -0,0 +1,12 @@
+package com.java365;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day011Test {
+    @Test
+    public void testGreeting(){
+        assertEquals("java-365第11天",Day011.greeting());
+    }
+}
