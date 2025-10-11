c518174253a3b1a17c63e4df0bfa40f99916639b
lijiachi <18461031174@163.com>
2025-10-11 20:36:58 +0800

    养成Day012习惯

---
+++ src/main/java/com/java365/Day012.java
@@ -0,0 +1,7 @@
+package com.java365;
+
+public class Day012 {
+    public static String greeting(){
+       return "java-365第12天";
+    }
+}
---
+++ src/test/java/com/java365/Day012Test.java
@@ -0,0 +1,13 @@
+package com.java365;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day012Test {
+    @Test
+    public void testGreeting(){
+        assertEquals("java-365第12天",Day011.greeting());
+//        assertEquals.("java-365第12天",Day012.greeting());我当时看了许久也没发现错误
+    }
+}
